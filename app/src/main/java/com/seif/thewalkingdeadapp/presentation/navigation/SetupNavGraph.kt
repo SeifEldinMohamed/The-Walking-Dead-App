95fc46d00032602c5e8ddcead3578d85ae9411bc
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-01-03 21:02:51 +0200

    - Design SplashScreen.kt

--- app/src/main/java/com/seif/thewalkingdeadapp/presentation/navigation/SetupNavGraph.kt
+++ app/src/main/java/com/seif/thewalkingdeadapp/presentation/navigation/SetupNavGraph.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.seif.thewalkingdeadapp.presentation.screens.splash.SplashScreen
 import com.seif.thewalkingdeadapp.utils.Constants.DETAILS_ARGUMENT_KEY
 
 @Composable
@@ -15,7 +16,7 @@ fun SetupNavGraph(navController: NavHostController) {
         startDestination = Screen.Splash.route
     ) {
         composable(route = Screen.Splash.route) {
-            // will pass our composable screens
+            SplashScreen(navController = navController)
         }
         composable(route = Screen.Welcome.route) {
 
