253c8bcabee787fabc24ef9e88166378715b33d3
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-01-12 19:31:33 +0200

    create ApiResponse.kt and TheWalkingDeadApi.kt

---
+++ app/src/main/java/com/seif/thewalkingdeadapp/data/remote/TheWalkingDeadApi.kt
@@ -0,0 +1,19 @@
+package com.seif.thewalkingdeadapp.data.remote
+
+import com.seif.thewalkingdeadapp.data.remote.dto.ApiResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TheWalkingDeadApi {
+
+    @GET("/heroes")
+    suspend fun getAllCharacters(
+        @Query("page") page: Int = 1
+    ): ApiResponse
+
+    @GET("/character/search")
+    suspend fun searchHeroes(
+        @Query("name") name: String
+    ): ApiResponse
+
+}
