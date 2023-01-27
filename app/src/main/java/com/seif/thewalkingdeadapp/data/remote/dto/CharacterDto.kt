87c6fdfb7973863f1b68b0d463dd898a22a4b626
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-01-27 22:54:25 +0200

    CharacterDto.kt added getAllCharacters in RemoteDataSourceImpl

--- app/src/main/java/com/seif/thewalkingdeadapp/data/remote/dto/CharacterDto.kt
+++ app/src/main/java/com/seif/thewalkingdeadapp/data/remote/dto/CharacterDto.kt
@@ -1,10 +1,9 @@
 package com.seif.thewalkingdeadapp.data.remote.dto
 
-import androidx.room.PrimaryKey
 import kotlinx.serialization.Serializable
+
 @Serializable
 data class CharacterDto(
-    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val name: String,
     val realName: String,
--- app/src/main/java/com/seif/thewalkingdeadapp/data/repository/MainRepositoryImp.kt
+++ app/src/main/java/com/seif/thewalkingdeadapp/data/repository/MainRepositoryImp.kt
@@ -1,13 +1,29 @@
 package com.seif.thewalkingdeadapp.data.repository
 
+import androidx.paging.PagingData
+import androidx.paging.map
+import com.seif.thewalkingdeadapp.data.mapper.toMyCharacter
+import com.seif.thewalkingdeadapp.domain.model.MyCharacter
 import com.seif.thewalkingdeadapp.domain.repository.DataStoreOperations
 import com.seif.thewalkingdeadapp.domain.repository.MainRepository
+import com.seif.thewalkingdeadapp.domain.repository.RemoteDataSource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class MainRepositoryImp @Inject constructor(
-    private val dataStore: DataStoreOperations
-): MainRepository {
+    private val dataStore: DataStoreOperations,
+    private val remote: RemoteDataSource
+) : MainRepository {
+
+    fun getAllCharacters(): Flow<PagingData<MyCharacter>> {
+        return remote.getAllHeroes().map {
+            it.map { characterEntity ->
+                characterEntity.toMyCharacter()
+            }
+        }
+    }
+
 
     override suspend fun saveOnBoardingState(completed: Boolean) {
         dataStore.saveOnBoardingState(completed = completed)
