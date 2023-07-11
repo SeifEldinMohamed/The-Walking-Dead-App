70e6d8b2bd7fe7efcb673a3a0e8f95bbcba8891c
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-07-11 20:11:48 +0300

    add ui/theme

--- app/src/main/java/com/seif/thewalkingdeadapp/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/seif/thewalkingdeadapp/presentation/ui/theme/Type.kt
@@ -1,10 +1,13 @@
 package com.seif.thewalkingdeadapp.presentation.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.seif.thewalkingdeadapp.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -26,3 +29,7 @@ val Typography = Typography(
     )
     */
 )
+
+val characterPlaceHolderImage: Int
+    @Composable
+    get() = if (isSystemInDarkTheme()) R.drawable.character_placeholder_dark else R.drawable.character_placeholder_light
