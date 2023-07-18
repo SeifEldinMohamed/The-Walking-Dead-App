9dc228722c7332edb529b45b49bf2e8c37338c1c
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-07-18 11:38:32 +0300

    adjust some code in SetupNavGraph.kt

--- app/src/main/java/com/seif/thewalkingdeadapp/presentation/navigation/Screen.kt
+++ app/src/main/java/com/seif/thewalkingdeadapp/presentation/navigation/Screen.kt
@@ -1,10 +1,12 @@
 package com.seif.thewalkingdeadapp.presentation.navigation
 
+import com.seif.thewalkingdeadapp.utils.Constants.DETAILS_ARGUMENT_KEY
+
 sealed class Screen(val route: String) {
     object Splash : Screen("splash_screen")
     object Welcome : Screen("welcome_screen")
     object Home : Screen("home_screen")
-    object Details : Screen("details_screen/{heroId}") {
+    object Details : Screen("details_screen/{${DETAILS_ARGUMENT_KEY}}") {
         fun passHeroId(heroId: Int): String {
             return "details_screen/$heroId"
         }
--- app/src/main/java/com/seif/thewalkingdeadapp/presentation/navigation/SetupNavGraph.kt
+++ app/src/main/java/com/seif/thewalkingdeadapp/presentation/navigation/SetupNavGraph.kt
@@ -3,29 +3,49 @@ package com.seif.thewalkingdeadapp.presentation.navigation
 import WelcomeScreen
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import coil.annotation.ExperimentalCoilApi
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.seif.thewalkingdeadapp.presentation.screens.home.HomeScreen
 import com.seif.thewalkingdeadapp.presentation.screens.splash.SplashScreen
+import com.seif.thewalkingdeadapp.presentation.screens.splash.SplashViewModel
+import com.seif.thewalkingdeadapp.presentation.screens.welcome.WelcomeViewModel
 import com.seif.thewalkingdeadapp.utils.Constants.DETAILS_ARGUMENT_KEY
 
+@ExperimentalCoilApi
 @ExperimentalAnimationApi
 @ExperimentalPagerApi
 @Composable
-fun SetupNavGraph(navController: NavHostController) {
+fun SetupNavGraph(navController: NavHostController) { // we will pass this navHostController form mainActivity
     NavHost(
         navController = navController,
         startDestination = Screen.Splash.route
     ) {
         composable(route = Screen.Splash.route) {
-            SplashScreen(navController = navController)
+            SplashScreen(
+                onSplashFinished = { onBoardingCompleted ->
+                    navController.popBackStack() // to remove splash screen from backStack
+                    if (onBoardingCompleted)
+                        navController.navigate(Screen.Home.route)
+                    else
+                        navController.navigate(Screen.Welcome.route)
+                }
+            )
         }
         composable(route = Screen.Welcome.route) {
-            WelcomeScreen(navController = navController)
+            val welcomeViewModel: WelcomeViewModel = hiltViewModel()
+            WelcomeScreen(
+                onFinishButtonClicked = {
+                    welcomeViewModel.saveOnBoardingState(completed = true)
+                    navController.popBackStack()
+                    navController.navigate(Screen.Home.route)
+                }
+            )
         }
         composable(route = Screen.Home.route) {
             HomeScreen()
